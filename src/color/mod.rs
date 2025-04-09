ee20567202e44d7311eac0ab5c4b205d0f25d1d8
Daniel Gallups <dsgallups@protonmail.com>
2025-04-09 08:00:43 -0400

    add subcommand

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -1,16 +1,77 @@
 #![doc = include_str!("README.md")]
 
-use anyhow::Result;
 use quote::quote;
 use tailwind::TailwindMap;
 mod ir;
 mod tailwind;
+use anyhow::Result;
+use std::{
+    fs::{self, File},
+    io::{self, Write},
+};
+
+use clap::Parser;
+#[derive(Parser, Debug, Default)]
+#[command(version, about)]
+pub struct ColorArgs {
+    #[arg(short, long)]
+    pub input_file: Option<String>,
+    #[arg(short, long)]
+    pub output_file: Option<String>,
+}
+impl ColorArgs {
+    pub fn new(input_file: impl ToString, output_file: impl ToString) -> Self {
+        Self {
+            input_file: Some(input_file.to_string()),
+            output_file: Some(output_file.to_string()),
+        }
+    }
+}
+
+/// The main entry point for converting a file with the CLI tool.
+///
+/// If you don't need to open files/use stdin, then see [`color::generate`].
+pub fn handle_args(args: ColorArgs) -> Result<()> {
+    let input = match args.input_file {
+        Some(file) => fs::read_to_string(file)?,
+        None => loop {
+            let mut input = String::new();
+            println!("Paste your JSON [press ENTER to continue]:");
+            if io::stdin().read_line(&mut input).is_err() {
+                println!("Failed to read input.");
+                continue;
+            }
+            break input;
+        },
+    };
+    let result = generate(&input)?;
+
+    if let Some(output_path) = args.output_file {
+        let Ok(mut of) = File::create(output_path) else {
+            println!("Error creating file at path. Generated Output:\n=====\n{result}\n=====");
+            return Ok(());
+        };
+
+        if let Err(e) = of.write_all(result.to_string().as_bytes()) {
+            println!(
+                "Error at path:\n{:?}\nGenerated Output:\n=====\n{result}\n=====",
+                e
+            );
+            return Ok(());
+        };
+        println!("Result written to output.");
+
+        return Ok(());
+    }
+    println!("Output\n=====\n{result}\n=====");
+    Ok(())
+}
 
 /// Takes in a JSObject or JSON and returns file contents
 pub fn generate(input: &str) -> Result<String> {
     let val = TailwindMap::from_json(input)?;
     let header = quote! {
-        /// Generated using `color-gen` v0.2
+        /// Generated using `bevygen` v0.1
 
         use bevy::color::Color;
     };
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use anyhow::Result;
+use bevygen::color::ColorArgs;
 use clap::{Parser, Subcommand};
-use tracing::level_filters::LevelFilter;
+//use tracing::level_filters::LevelFilter;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Tracing verbosity
-    #[arg(global = true, short, long, value_enum, default_value = "ERROR")]
-    log: LevelFilter,
-
+    // /// Tracing verbosity
+    // #[arg(global = true, short, long, value_enum, default_value = "ERROR")]
+    // log: LevelFilter,
     #[command(subcommand)]
     command: Command,
 }
@@ -16,16 +17,29 @@ enum Command {
     /// Generate a color file from a JSON or JSObject
     ///
     /// Recommended to project an input json.
-    ///
-    /// This will probably be removed
-    Color {
-        #[arg(short, long)]
-        input_file: Option<String>,
-        #[arg(short, long)]
-        output_file: Option<String>,
-    },
+    Color(ColorArgs),
+    // /// Scaffold a set of files
+    // #[command(subcommand)]
+    // Scaffold(ScaffoldCommand),
 }
 
-fn main() {
+// #[derive(Subcommand)]
+// enum ScaffoldCommand {
+//     /// Generates a palette directory
+//     Palette {
+//         #[arg(short, long, default_value = ".")]
+//         path: PathBuf,
+
+//         /// The name of the palette directory
+//         #[arg(short, long, default_value = "palettes")]
+//         name: String,
+//     },
+// }
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
+
+    match cli.command {
+        Command::Color(args) => bevygen::color::handle_args(args),
+    }
 }
