bc160e8583c1d3b7530e64b4e44bf81ca2577392
Daniel Gallups <dsgallups@protonmail.com>
2025-04-09 07:36:26 -0400

    add initial command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-use anyhow::Result;
-use bevygen::{Args, handle_args};
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use tracing::level_filters::LevelFilter;
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Tracing verbosity
+    #[arg(global = true, short, long, value_enum, default_value = "ERROR")]
+    log: LevelFilter,
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-    handle_args(args)
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Generate a color file from a JSON or JSObject
+    ///
+    /// Recommended to project an input json.
+    ///
+    /// This will probably be removed
+    Color {
+        #[arg(short, long)]
+        input_file: Option<String>,
+        #[arg(short, long)]
+        output_file: Option<String>,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
 }
